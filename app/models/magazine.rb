8ba3ec8f827c6a8410fa7ff50a83edbb6d04b514
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-10-30 17:02:54 +0300

    completed code challenge

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,39 @@
 class Magazine
   attr_accessor :name, :category
-
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+    end
 
-end
+    def articles 
+      Article.all.filter{|article| article.magazine.name == @name}
+      end
+
+
+    def contributors
+     self.articles.map { |article|article.author}.uniq
+        end
+
+
+    def self.find_by_name (name)
+      self.all.find  {|magazine| magazine.name == name} 
+  end
+
+  
+    def article_titles
+    self.articles.map { | article | article.title}
+    end
+    
+
+    def contributing_authors 
+      self.contributors.filter { |author| author.articles.count > 2}
+      end
+    end
+    
--- solution.rb
+++ solution.rb
@@ -1,1 +1,81 @@
 # Please copy/paste all three classes into this file to submit your solution!
+# class Article
+class Article
+    @@all = []
+    attr_reader :magazine, :title 
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+ 
+    end
+
+    def author 
+        @author.name
+    end
+
+    def self.all
+        @@all
+end
+
+
+end
+
+# class author
+class Author
+    attr_reader :name
+  
+    def initialize(name)
+      @name = name
+    end
+  
+  
+    def articles 
+      Article.all.filter {|article| article.author == @name}
+      end
+  
+    def magazines 
+    articles.map { |article| article.magazine }.uniq
+    end
+  
+    def add_article(magazine, title)
+      Article.new(magazine, self, title)
+    end
+  
+    def topics_areas
+      magazines.map { |magazine| magazine.category }.uniq
+   end
+  
+  end
+  
+# class magazine
+class Author
+    attr_reader :name
+  
+    def initialize(name)
+      @name = name
+    end
+  
+  
+    def articles 
+      Article.all.filter {|article| article.author == @name}
+      end
+  
+    def magazines 
+    articles.map { |article| article.magazine }.uniq
+    end
+  
+    def add_article(magazine, title)
+      Article.new(magazine, self, title)
+    end
+  
+    def topics_areas
+      magazines.map { |magazine| magazine.category }.uniq
+   end
+  
+  end
+  
+  
+  
