998d658ecf3f64d926e47e07b6df948bf44ff53a
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-10-30 17:21:53 +0300

    done

--- tools/console.rb
+++ tools/console.rb
@@ -6,14 +6,25 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+author_1=Author.new("Thierry Henry")
+author_2=Author.new("Steve Wozniak")
+
+#Magazine
+sports1=Magazine.new("EPL", "Football")
+sport2=Magazine.new("Sevens Series", "Rugby")
+tech1=Magazine.new("World of Tech", "Technology")
 
 
 
-
-
-
-
-
+#Article
+article1=Article.new(author_1, sports1, "This is my article")
+article2=Article.new(author_2, tech1, "This is my tech article")
+article2=Article.new(author_1, sports1, "This is my second article")
+article4=Article.new(author_2, tech1, "This is about JS")
+article5=Article.new(author_1, sport2, "This is about racing")
+article6=Article.new(author_2, tech1, "This is about ruby")
+article7=Article.new(author_2, tech1, "This is about JS")
+article8=Article.new(author_2, tech1, "This is about Python")
 
 ### DO NOT REMOVE THIS
 binding.pry
